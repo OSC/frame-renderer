e69bc281ac97b01071f10e11a37412f6e88dfc7d
Jeff Ohrstrom <johrstrom@hotmail.com>
2019-08-15 14:53:05 -0400

    dont mk project directories and allow users to select them from filepicker

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -16,11 +16,7 @@ class ProjectsController < ApplicationController
 
   def create
     @project = Project.new(project_params)
-    init_project_dir @project
     @project.save
-
-    make_project_dir @project.directory
-
     redirect_to @project
   end
 
@@ -54,18 +50,5 @@ class ProjectsController < ApplicationController
     params
       .require(:project)
       .permit(:name, :description, :directory)
-  end
-
-  def init_project_dir prj
-    if prj.directory.blank?
-      prj.directory =
-        ENV['HOME'] +
-        '/ondemand/data/dev/video-processing/' +
-        prj.name
-    end
-  end
-
-  def make_project_dir dir
-    FileUtils.mkdir_p dir unless File.directory?(dir)
   end
 end
