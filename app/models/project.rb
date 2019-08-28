c86f3c5dde65a4e84b57af5dfc6859b69737b308
Jeff Ohrstrom <johrstrom@hotmail.com>
2019-08-28 17:14:16 -0400

    #2

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,22 @@
 class Project < ActiveRecord::Base
   validates :name, uniqueness: true, presence: true, length: { minimum: 5 }
   has_many :submissions
+
+  class << self
+    def maya_top_level_dir
+      dir = Pathname.new(ENV['HOME']).join('maya', 'projects')
+      dir.mkpath unless dir.exist?
+      dir.to_s
+    end
+  end
+
+  def initialize
+    super
+    write_attribute(:directory, Project.maya_top_level_dir)
+  end
+
+  def initialize(params)
+    super
+  end
+
 end
