51373132883dc295200d0eeea9b0fe732de86fcf
Jeff Ohrstrom <johrstrom@hotmail.com>
2019-08-29 15:04:34 -0400

    rebrand to frame-renderer

--- config/boot.rb
+++ config/boot.rb
@@ -5,5 +5,5 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 
 ENV['OOD_DATAROOT'] = Pathname.new(ENV['HOME'])
-                              .join('ondemand', 'data', 'video-processing')
+                              .join('ondemand', 'data', 'frame-renderer')
                               .expand_path.to_s
