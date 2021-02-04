ad1b6dc49ddc4aaeef8a1fc99b05020de2e1dbbd
Jeff Ohrstrom <johrstrom@hotmail.com>
2021-02-04 13:23:49 -0500

    update to rails 5.1 and rm raise_in_transactional_callbacks config

--- config/application.rb
+++ config/application.rb
@@ -20,10 +20,6 @@ module FrameRenderer
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
-
-
     # new in rails 5.0
     config.active_record.belongs_to_required_by_default = true
     config.action_controller.per_form_csrf_tokens = true
