ba8ccb8de17be924856420f2938e72f01c8720b1
Bibhus Luitel <bluitel@osc.edu>
2021-06-15 13:00:20 -0400

    Creation test with database entry

--- test/integration/projects_test.rb
+++ test/integration/projects_test.rb
@@ -2,67 +2,24 @@ require 'test_helper'
 
 class ProjectsTest < ActionDispatch::IntegrationTest
   test "testing_action_create" do
+    before = Project.all.size
     Dir.mktmpdir do |tmpdir|
-      params = {
-        project: { name: 
-                    "test project one", 
-                   description: 
-                    "test project one description", 
-                   directory: 
-                    tmpdir
-        }
-      }
-      post projects_path, params: params
-      follow_redirect!
-      assert_response :success
-      get projects_path
-      assert_response :success
-    
+    params = {
+      project: {name: 
+                  "test project one", 
+                description: 
+                  "test project one description", 
+                directory: 
+                  tmpdir
+                }
+              }
+    post projects_path, params: params
+    follow_redirect!
+    assert_response :success
+    get projects_path
+    assert_response :success
+    after = Project.all.size
+    assert after > before
+    end
   end
-end
-  test "cannot_create_without_valid_directory" do
-  params = {
-    project: { name: 
-                "test project two", 
-               description: 
-                "test project two description", 
-               directory: 
-                "tmpdir/"
-    }
-  }
-  post projects_path, params: params
-  !assert_response :success
-  get projects_path
-  assert_not :success
-  end
-
-  test "cannot_create_two_projects_with_same_name" do
-    Dir.mktmpdir do |tmpdir|
-      params = {
-        project: { name: 
-                    "test project three", 
-                   description: 
-                    "test project three description", 
-                   directory: 
-                    tmpdir
-        }
-      }
-      post projects_path, params: params
-      follow_redirect!
-      assert_response :success
-      get projects_path
-      assert_response :success
-      params2 = {
-        project: { name: 
-                    "test project three", 
-                   description: 
-                    "test project three description", 
-                   directory: 
-                    tmpdir
-        }
-      }
-      post projects_path, params: params
-      !assert_response :success
-  end
-end
 end
