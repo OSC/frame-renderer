d627a0738c1d47e6fc514d84bb91fa38e31bf00d
Bibhus Luitel <bluitel@osc.edu>
2021-06-27 14:35:40 -0400

    formatting

--- test/integration/projects_test.rb
+++ test/integration/projects_test.rb
@@ -5,60 +5,79 @@ class ProjectsTest < ActionDispatch::IntegrationTest
     before = Project.all.size
     Dir.mktmpdir do |tmpdir|
       params = {
-        project: {name: 
-                  "test project one", 
-                description: 
-                  "test project one description", 
-                directory: 
-                  tmpdir
+        project: {
+            name: "test project one",
+            description: "test project one description",
+            directory: tmpdir
                 }
               }
       post projects_path, params: params
       follow_redirect!
       assert_response :success
+
       get projects_path
       assert_response :success
+
       after = Project.all.size
-      assert after = before + 1
-      end
+      assert after == before + 1
+    end
   end
+
   test "deleting_a_project" do
     before = Project.all.size
     Dir.mktmpdir do |tmpdir|
       params = {
-        project: {name: 
-                  "test project one", 
-                description: 
-                  "test project one description", 
-                directory: 
-                  tmpdir
-                }
+        project: {
+            name: "test project one",
+            description: "test project one description",
+            directory: tmpdir
+          }
               }
       post projects_path, params: params
+
       id = @response.location.to_s.split('/').last
+
       delete project_path(id)
       follow_redirect!
       assert_response :success
+
       after = Project.all.size
-      assert after = before - 1
-      end
+      assert after = before -1
+    end
   end
   test "update a project" do
-    Dir.mktmpdir do |tmpdir|
-      name = "update project"
-      description = "test for updating a project"
-      directory = tmpdir
-      post projects_path, params: {project: {name: name, description: description, directory: directory}}
-      id = @response.location.to_s.split('/').last
-      get edit_project_path(id)
-      assert_response :success
-      before = Project.all.size
-      put project_path(id), params: {project: {name: "new name", description: "new description", directory: "same stuff"}}
-      follow_redirect!
-      assert_response :success
-      after = Project.all.size
-      assert before == after
-    end
+    Dir.mktmpdir do |tmpdir2|
+      Dir.mktmpdir do |tmpdir|
+        params = {
+          project: {
+            name: "update project",
+            desccription: "project description",
+            directory: tmpdir
+          }
+        }
+        new_params = {
+          project: {
+            name: "new name",
+            desccription: "new description",
+            directory: tmpdir2
+          }
+        }
+        post projects_path, params: params
+      
+        id = @response.location.to_s.split("/").last
+      
+        get edit_project_path(id)
+        assert_response :success
 
+        before = Project.all.size
+
+        put project_path(id), params: new_params
+        follow_redirect!
+        assert_response :success
+
+        after = Project.all.size
+        assert_equal(before, after)
+      end
+    end
   end
 end
