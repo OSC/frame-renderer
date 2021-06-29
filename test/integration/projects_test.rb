c25e0ad60bbe21fe3a3a7289728b2b96d998bdab
Bibhus Luitel <69481703+maharajabhai@users.noreply.github.com>
2021-06-29 13:14:59 -0400

    test for major crud operations on projects (#71)
    * test for major crud operations on projects
    
    * formatting
    
    * more formatting work
    
    * removed unnecessary tests

---
+++ test/integration/projects_test.rb
@@ -0,0 +1,83 @@
+require "test_helper"
+
+class ProjectsTest < ActionDispatch::IntegrationTest
+  test "create" do
+    before = Project.all.size
+    Dir.mktmpdir do |tmpdir|
+      params = {
+        project: {
+          name: "test project one",
+          description: "test project one description",
+          directory: tmpdir
+        }
+      }
+      post projects_path, params: params
+      follow_redirect!
+      assert_response :success
+
+      get projects_path
+      assert_response :success
+
+      after = Project.all.size
+      assert after == before + 1
+    end
+  end
+
+  test "deleting_a_project" do
+    before = Project.all.size
+    Dir.mktmpdir do |tmpdir|
+      params = {
+        project: {
+          name: "test project one",
+          description: "test project one description",
+          directory: tmpdir
+        }
+      }
+      post projects_path, params: params
+
+      id = @response.location.to_s.split("/").last
+
+      delete project_path(id)
+      follow_redirect!
+      assert_response :success
+
+      after = Project.all.size
+      assert after = before -1
+    end
+  end
+  test "update a project" do
+    Dir.mktmpdir do |tmpdir2|
+      Dir.mktmpdir do |tmpdir|
+        params = {
+          project: {
+            name: "update project",
+            desccription: "project description",
+            directory: tmpdir
+          }
+        }
+        new_params = {
+          project: {
+            name: "new name",
+            desccription: "new description",
+            directory: tmpdir2
+          }
+        }
+        post projects_path, params: params
+      
+        id = @response.location.to_s.split("/").last
+      
+        get edit_project_path(id)
+        assert_response :success
+
+        before = Project.all.size
+
+        put project_path(id), params: new_params
+        follow_redirect!
+        assert_response :success
+
+        after = Project.all.size
+        assert_equal(before, after)
+      end
+    end
+  end
+end
