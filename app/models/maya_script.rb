a7e7f57f5db5ba784322c3bf533916adc453e013
Jeff Ohrstrom <johrstrom@osc.edu>
2021-09-07 14:31:45 -0400

    maya script needs to have model_name too

--- app/models/maya_script.rb
+++ app/models/maya_script.rb
@@ -2,6 +2,10 @@
 
 # Script subclass for Maya rendering
 class MayaScript < Script
+  def self.model_name
+    Script.model_name
+  end
+
   def script_template
     Configuration.maya_script_template
   end
