e917284e2ab176a68b4470ee634ee5cdf6711d02
Jeff Ohrstrom <johrstrom@osc.edu>
2021-09-08 12:24:17 -0400

    support vray project types (#90)

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,12 +4,17 @@ class Project < ApplicationRecord
   validate :directory_must_be_valid
   validates_uniqueness_of :name
   has_many :scripts
-  self.inheritance_column = :type
 
   # add accessors: [ :attr1, :attr2 ] etc. when you want to add getters and
   # setters to add new attributes stored in the JSON store
   # don't remove attributes from this list going forward! only deprecate
   store :project_attrs, coder: JSON, accessors: %i[]
+
+  # we use `project_type` to allow for the form to set this attr and use _it_
+  # to determine the `type` which will instantiate subclasses.
+  attr_accessor :project_type
+
+  self.inheritance_column = :type
 
   def directory_must_be_valid
     unless Dir.exist? directory
--- test/integration/projects_test.rb
+++ test/integration/projects_test.rb
@@ -1,13 +1,15 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class ProjectsTest < ActionDispatch::IntegrationTest
-  test "create" do
+  test 'create' do
     before = Project.all.size
     Dir.mktmpdir do |tmpdir|
       params = {
         project: {
-          name: "test project one",
-          description: "test project one description",
+          name: 'test project one',
+          description: 'test project one description',
           directory: tmpdir
         }
       }
@@ -22,53 +24,52 @@ class ProjectsTest < ActionDispatch::IntegrationTest
       assert after == before + 1
     end
   end
-  
-  test "cannot create same name" do
+
+  test 'cannot create same name' do
     Dir.mktmpdir do |tmpdir|
       params = {
         project: {
-          name: "test project one",
-          description: "test project one description",
+          name: 'test project one',
+          description: 'test project one description',
           directory: tmpdir
         }
       }
       post projects_path, params: params
-      id = @response.location.to_s.split("/").last
+      id = @response.location.to_s.split('/').last
       assert_redirected_to project_path(id)
-      
-      assert_difference("Project.count", 0) do
+
+      assert_difference('Project.count', 0) do
         post projects_path, params: params
       end
     end
   end
 
-  test "cannot create invalid directory" do
+  test 'cannot create invalid directory' do
     params = {
       project: {
-        name: "test project one",
-        description: "test project one description",
-        directory: "tmpdir"
+        name: 'test project one',
+        description: 'test project one description',
+        directory: 'tmpdir'
       }
     }
-    assert_difference("Project.count", 0) do
+    assert_difference('Project.count', 0) do
       post projects_path, params: params
     end
   end
 
-  test "deleting_a_project" do
-
+  test 'deleting_a_project' do
     Dir.mktmpdir do |tmpdir|
       params = {
         project: {
-          name: "test project one",
-          description: "test project one description",
+          name: 'test project one',
+          description: 'test project one description',
           directory: tmpdir
         }
       }
       post projects_path, params: params
 
-      id = @response.location.to_s.split("/").last
-      assert_difference("Project.count", -1) do
+      id = @response.location.to_s.split('/').last
+      assert_difference('Project.count', -1) do
         delete project_path(id)
       end
 
@@ -77,27 +78,27 @@ class ProjectsTest < ActionDispatch::IntegrationTest
     end
   end
 
-  test "update a project" do
+  test 'update a project' do
     Dir.mktmpdir do |tmpdir2|
       Dir.mktmpdir do |tmpdir|
         params = {
           project: {
-            name: "update project",
-            desccription: "project description",
+            name: 'update project',
+            desccription: 'project description',
             directory: tmpdir
           }
         }
         new_params = {
           project: {
-            name: "new name",
-            desccription: "new description",
+            name: 'new name',
+            desccription: 'new description',
             directory: tmpdir2
           }
         }
         post projects_path, params: params
-      
-        id = @response.location.to_s.split("/").last
-      
+
+        id = @response.location.to_s.split('/').last
+
         get edit_project_path(id)
         assert_response :success
 
@@ -107,9 +108,7 @@ class ProjectsTest < ActionDispatch::IntegrationTest
         follow_redirect!
         assert_response :success
 
-        
-        assert_equal("new name", Project.find(id).name)
-
+        assert_equal('new name', Project.find(id).name)
 
         after = Project.all.size
         assert_equal(before, after)
@@ -129,11 +128,54 @@ class ProjectsTest < ActionDispatch::IntegrationTest
       }
 
       post projects_path, params: params
-      id = @response.location.to_s.split("/").last
+      id = @response.location.to_s.split('/').last
 
       follow_redirect!
       assert_response :success
 
+      assert Project.find(id).is_a?(MayaProject)
+    end
+  end
+
+  test 'creates a vray project' do
+    Dir.mktmpdir do |tmpdir|
+      params = {
+        project: {
+          name: 'test vray',
+          description: 'test project one description',
+          directory: tmpdir,
+          project_type: 'VRay'
+        }
+      }
+
+      post projects_path, params: params
+      id = @response.location.to_s.split('/').last
+
+      follow_redirect!
+      assert_response :success
+
+      assert Project.find(id).is_a?(VRayProject)
+    end
+  end
+
+  test 'handles bad project type input' do
+    Dir.mktmpdir do |tmpdir|
+      params = {
+        project: {
+          name: 'test bad input',
+          description: 'test project one description',
+          directory: tmpdir,
+          project_type: 'some other thing'
+        }
+      }
+
+      post projects_path, params: params
+      id = @response.location.to_s.split('/').last
+
+      follow_redirect!
+      assert_response :success
+
+      # defaults to creating a MayaProject
       assert Project.find(id).is_a?(MayaProject)
     end
   end
