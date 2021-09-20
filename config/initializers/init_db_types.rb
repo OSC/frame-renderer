95b66f1096623121e5c52eb77bbb8f69268b72b6
Jeff Ohrstrom <johrstrom@osc.edu>
2021-09-20 13:22:37 -0400

    safegaurd modifying columns when they may not exist (#105)

--- config/initializers/init_db_types.rb
+++ config/initializers/init_db_types.rb
@@ -6,7 +6,12 @@ rails_env = ENV['RAILS_ENV']
 db_cfg = Rails.configuration.database_configuration[rails_env]
 db = db_cfg.nil? ? '' : db_cfg['database']
 
-if File.exist?(db) && !File.zero?(db)
+def columns_exist?
+  ActiveRecord::Base.connection.column_exists?(:projects, :type) &&
+    ActiveRecord::Base.connection.column_exists?(:scripts, :type)
+end
+
+if File.exist?(db) && !File.zero?(db) && columns_exist?
   Project.where(type: nil).update_all(type: 'MayaProject')
   Script.where(type: nil).update_all(type: 'MayaScript')
 end
