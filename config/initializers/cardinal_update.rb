132760a5eadec3049056851396129e33bcf21a54
Jeff Ohrstrom <johrstrom@osc.edu>
2025-01-07 13:29:49 -0500

    rescue this in case it fails

--- config/initializers/cardinal_update.rb
+++ config/initializers/cardinal_update.rb
@@ -4,10 +4,14 @@
 # Cardinal is 2025, so update all the records for the user
 # as whatever version they have in the DB won't work.
 Rails.application.config.after_initialize do
-  Script.all.each do |script|
-    if script.is_a?(MayaScript)
-      script.version = '2025' if script.version.to_s < '2025'
-      script.save
+  begin
+    Script.all.each do |script|
+      if script.is_a?(MayaScript)
+        script.version = '2025' if script.version.to_s < '2025'
+        script.save
+      end
     end
+  rescue
+    # nothing to do, no database table exists.
   end
 end
